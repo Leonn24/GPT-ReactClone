e9a80a44628a5604353994ba5ea366c8654e162f
Connor Johnson <connorjohnson@connors-air.mynetworksettings.com>
2024-04-05 00:51:35 -0400

    Added basic login functionality as well as the skeleton for user authentication

--- gpt-reactclone/src/App.jsx
+++ gpt-reactclone/src/App.jsx
@@ -1,55 +1,11 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import Chatbox from './components/Chatbox';
-import AnswerSection from './components/Answer';
-import OpenAI from "openai";
-import './App.css';
-import './normal.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [messages, setMessages] = useState([]);
-
-  const openai = new OpenAI({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true
-  });
-  
-  const responseGenerate = async (inputText, setInputText) => {
-    let option = {
-      model: "gpt-3.5-turbo-instruct",
-      prompt: 'Complete this sentence: "${inputText}"',
-      temperature: 1,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    };
-
-    let completeOptions = {
-      ...option,
-      prompt: inputText,
-    };
-    
-    const response = await openai.completions.create(completeOptions);
-    if (response.choices.length) {
-      setMessages([
-        {
-          question: inputText,
-          answer: response.choices[0].text,
-        },
-        ...messages,
-      ]);
-      setInputText('');
-    }
-  };
-
   return (
     <>
-      <div>
-        <Header />
-        <Chatbox responseGenerate={responseGenerate}/>
-        <AnswerSection messages={messages}/>
-      </div>
+      <main className="mx-3">
+        <Outlet />
+      </main>
     </>
   );
 }
--- gpt-reactclone/src/pages/Homepage.jsx
+++ gpt-reactclone/src/pages/Homepage.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import Chatbox from '../components/Chatbox';
+import AnswerSection from '../components/Answer';
+import OpenAI from "openai";
+import '../App.css';
+import '../normal.css';
 
 function Homepage() {
+  const [messages, setMessages] = useState([]);
+
+  const openai = new OpenAI({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true
+  });
+  
+  const responseGenerate = async (inputText, setInputText) => {
+    let option = {
+      model: "gpt-3.5-turbo-instruct",
+      prompt: 'Complete this sentence: "${inputText}"',
+      temperature: 1,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    };
+
+    let completeOptions = {
+      ...option,
+      prompt: inputText,
+    };
+    
+    const response = await openai.completions.create(completeOptions);
+    if (response.choices.length) {
+      setMessages([
+        {
+          question: inputText,
+          answer: response.choices[0].text,
+        },
+        ...messages,
+      ]);
+      setInputText('');
+    }
+  };
+
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
-  )
+    <>
+      <div>
+        <Header />
+        <Chatbox responseGenerate={responseGenerate}/>
+        <AnswerSection messages={messages}/>
+      </div>
+    </>
+  );
 }
 
-export default Homepage
+export default Homepage;
