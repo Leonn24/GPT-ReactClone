ed8351c56b8551cd0e6f2f0aaa1715c2ef74525a
Michael Salerno <mike.salano@gmail.com>
2024-04-08 19:19:11 -0400

    sidebar tings

--- gpt-reactclone/src/App.jsx
+++ gpt-reactclone/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import Homepage from  "./pages/Homepage";
+import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 import './App.css';
 import './normal.css';
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<Homepage />} />
             <Route path="/login" element={<Login />} />
           </Routes>
+          <Sidebar/>
         </Router>
       </div>
     </>
--- gpt-reactclone/src/components/Sidebar.jsx
+++ gpt-reactclone/src/components/Sidebar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../App.css'
 
+const testCode = ['resposne 1', 'response 2', 'response 3']
+
 function Sidebar() {
   return (
     <div className='Sidebar'>
-      Side
+      {
+        testCode.map((element, index) => {
+          return <div className='sidebarText' key={index}> {element} </div>
+        })
+      }
     </div>
   )
 }
